76f2339b4e96e645cfb7118e0580a95a93039798
MrGiveAFit <2r6ph8fkcw@privaterelay.appleid.com>
2026-02-18 09:09:17 -0600

    Update nba.js

--- api/nba.js
+++ api/nba.js
@@ -1,4 +1,13 @@
 export default async function handler(req, res) {
+  // CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   const { type, q, player_id, last_n = 10 } = req.query;
   const API_KEY = process.env.BALLDONTLIE_API_KEY;
 
